fb690bb9e1161b0aaf1006287cd7569c7396939a
pedrinvits <pedromantovani26@gmail.com>
2024-02-03 15:30:18 -0300

    feat - welcome menssage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,14 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
-
+import {format} from  "date-fns"
 export default function Home (){
     return (
-        <Header/>
+        <>
+            <Header/>
+            <div className="px-5 pt-5">
+                <h1 className="text-xl font-bold">Olá Pedro</h1>
+                <p className="capitalize text-sm">{format(new Date(), "EEEE ',' dd 'de' MMMM", {locale : ptBR})}</p>
+            </div>
+        </>
     )
 }
