1c42f8300023645ad920980ee225b7915c8778e1
pedrinvits <pedromantovani26@gmail.com>
2024-02-13 15:37:43 -0300

    feat : ensure user can search for barbershops

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,15 +1,58 @@
 "use client"
 import { Button } from "@/app/_components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/app/_components/ui/form";
 import { Input } from "@/app/_components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
+import { z } from "zod"
+import { useRouter } from "next/navigation";
+ 
+const formSchema = z.object({
+  search : z
+  .string({
+    required_error : "Campo Obrigatório",
+  })
+  .trim()
+  .min(1,"Campo Obrigatório")
+})
 const Search = () => {
+
+    const router = useRouter()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema)
+    })
+
+    const handleSubmit = (data: z.infer<typeof formSchema>) => {
+        router.push(`/barbershops?search=${data.search}`)
+    }
     return ( 
         <div className="flex items-center gap-2">
-            <Input placeholder="Busque uma barbearia"/>
-            <Button variant={'default'} size={'icon'}>
-                <SearchIcon size={20}/>
-            </Button>
+            <Form {...form}>
+                <form className="flex gap-4 w-full" onSubmit={form.handleSubmit(handleSubmit)}>
+                    
+                    
+                    <FormField
+                        control={form.control}
+                        name="search"
+                        render={({ field }) => (
+                            <FormItem>
+                           
+                            <FormControl>
+                                <Input placeholder="Busque uma barbearia" {...field}/>
+                            </FormControl>
+                            
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <Button variant={'default'} size={'icon'} type="submit">
+                        <SearchIcon size={20}/>
+                    </Button>
+                </form>
+            </Form>
         </div>
      );
 }
--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -20,6 +20,7 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
     where: {
       name: {
         contains: searchParams.search,
+        // nao considera maiuscula nem minuscula
         mode: "insensitive",
       },
     },
@@ -30,6 +31,13 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
       <Header />
       <div className="px-5 py-6 flex flex-col gap-6">
         <h1 className="text-gray-400 font-bold text-xs uppercase">Resultados para &quot;{searchParams.search}&quot;</h1>
+        <div className="grid grid-cols-2 gap-4">
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className="w-full">
+              <BarbershopItem barbershop={barbershop} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
