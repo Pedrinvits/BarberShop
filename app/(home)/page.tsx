ff4f270df87974e11baac2783205f36b37f1220c
pedro <pedrovitor.mantovani@yahoo.com.br>
2024-02-23 13:21:50 -0300

    feat : titles in home and scrollbar

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -67,7 +67,7 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        <h2 className="px-5 text-xl mb-3  text-gray-400 font-bold">Recomendados</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
@@ -79,7 +79,7 @@ export default async function Home() {
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        <h2 className="px-5 text-xl mb-3  text-gray-400 font-bold">Populares</h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {recommendedBarbershops.map((barbershop) => (
